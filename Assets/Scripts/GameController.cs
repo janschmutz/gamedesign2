39bfa82b5e2f3bfb7ac035938ce97b5f5e5634c6
Jan Schmutz <jan.schmutz@arcor.de>
2018-04-23 13:28:42 +0200

    init commit

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+	public GameObject gameOverPanel;
+	public Text scoreText;
+	public Text bestText;
+	public Text currText;
+
+	public GameObject newAlert;
+
+	int score = 0;
+	// Use this for initialization
+	void Start () {
+		Debug.Log (scoreText.text);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void GameOver() {
+		Invoke ("ShowOverPanel", 2.0f);
+
+	}
+	void ShowOverPanel() {
+		if (score > PlayerPrefs.GetInt ("Best", 0)) {
+			PlayerPrefs.SetInt ("Best", score);
+			newAlert.SetActive (true);
+		}
+		bestText.text = "Highscore: " + PlayerPrefs.GetInt ("Best", 0).ToString();
+		currText.text = "Current Score: " + score.ToString ();
+		scoreText.gameObject.SetActive (false);
+		gameOverPanel.SetActive (true);
+	}
+	public void Restart() {
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().name);
+	}
+	public void IncrementScore() {
+		score++;
+		scoreText.text = score.ToString();
+	}
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	public float jumpPower = 10.0f;
+	public float rotateSpeed = 1.0f;
+	Rigidbody2D myRigidbody;
+	bool isGrounded = false;
+	bool isGameOver = false;
+	float posX = 0.0f;
+	public float score = 0.0f;
+	ChallengeController myChallengeController;
+	GameController myGameController;
+
+	// Use this for initialization
+	void Start () {
+		myRigidbody = transform.GetComponent<Rigidbody2D> ();
+		posX = transform.position.x;
+		myChallengeController = GameObject.FindObjectOfType<ChallengeController> ();
+		myGameController = GameObject.FindObjectOfType<GameController> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Input.GetKey (KeyCode.Space) && isGrounded && !isGameOver) {
+			myRigidbody.AddForce (Vector3.up * (jumpPower * myRigidbody.mass * myRigidbody.gravityScale * 20.0f));
+		}
+		if (transform.position.x < posX) {
+			GameOver ();
+		}
+	}
+
+	void Update() {
+		
+	}
+
+	void GameOver() {
+		isGameOver = true;
+		myChallengeController.GameOver ();
+		Debug.Log ("Game Over");
+	}
+
+	void OnCollisionEnter2D(Collision2D other) {
+		if (other.collider.tag == "Ground") {
+			isGrounded = true;
+		}
+		if (other.collider.tag == "Enemy") {
+			GameOver ();
+		}
+	}
+	void OnCollisionStay2D(Collision2D other) {
+		if (other.collider.tag == "Ground") {
+			isGrounded = true;
+		}
+	}
+	void OnCollisionExit2D(Collision2D other) {
+		if (other.collider.tag == "Ground") {
+			isGrounded = false;
+		}
+	}
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.GetComponent<Collider2D>().tag == "Star") {
+			myGameController.IncrementScore ();
+		}
+	}
+
+}
